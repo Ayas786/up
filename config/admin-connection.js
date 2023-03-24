9baf08cf187e616c2a752a63064b44f1171eddfb
Ayas <ayasrehman786@gmail.com>
2023-03-25 00:55:53 +0530

    second

---
+++ config/admin-connection.js
@@ -0,0 +1,8 @@
+module.exports={
+
+    adminId:{
+email:"admin@gmail.com",
+// password:"$2a$10$6RRxEqDAkSDClDythMb92e6jMq3QV5jhWeHRqhYnK0LHGmDx7Rjj6"
+password:"$2a$10$JZu47goWvmEwwnpdUhqxX.uSzCoNE.00u22o6ii5gYQABlIWL0o9C"//admin1234
+    }
+}
---
+++ config/otp.js
@@ -0,0 +1,9 @@
+module.exports={
+    // serviceId:'VA4ecd548c33244b1e3ce54f1b88ce2f39',
+    serviceId:'VA4fea33dd8e72b850342c9f5cf16513e3',
+    // accountId:'ACf4eae9e9f6713ebcd3947591ea60088e',
+    accountId:'AC83d9c3e3061e8f7322d90bfed5ec2dcb',
+
+    // authToken:'9536c0c358a7201ba8a69626d7c8cc35',
+    authToken:'f5c9fd63466da1313347b343d2a89a31',
+}
